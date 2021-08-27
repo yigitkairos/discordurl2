9d7454ab295a323a27d67231c184ed660dcaa07a
yigitkairos <89653399+yigitkairos@users.noreply.github.com>
2021-08-27 18:55:23 +0300

    Add files via upload

---
+++ ayarlar.js
@@ -0,0 +1,16 @@
+const ayarlar = {
+  TOKEN: "", // ASSAGIYI OKU
+  sahip: "769727248391209032", //Doldurmak Zorunda Değilsiniz!
+  prefix: "!", //Bot Prefixi Yazın!
+  url: "yazik", //Alınmasını İstediğiniz Özel Discord Sunucu URL'sini Yazın!
+  sunucuID: "861439875080126474", //Url'nin Alınacağı Sunucu ID'sini Yazın!
+  log: "880826969103663154", //URL Alınır İse Bilgi Verilecek Log Kanal ID Yazın!
+  durum: "true", //false=Botu Durdur | true=Botu Başlat Demekdir!
+  botHızı: "1000" //1000 = 1 Saniye | 1 = 1 Milisaniye | En Az 50 Milisaniye Yazmanız Önerilir 50MS Altında Proje Discord Apiden Yasaklanır ve Çalşımayı Durdurur!
+};
+
+module.exports = ayarlar;
+
+//BOT TOKENINIZI EGER BOT GLITCHDE KALACAK
+//ISE .ENV DOSYASINA
+//EGER VDS YE KURACAK ISENIZ YUKARIYA 1. SATIRA YAZIN
